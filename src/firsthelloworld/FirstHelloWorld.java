07bfb6bd0a06fedc956e6bfb7f46db114597cebd
g13951te <g13951te@s30444.tsuda.ac.jp>
2015-04-10 11:11:23 +0900

    FirstHelloWorldです

---
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firsthelloworld;
+
+/**
+ *
+ * @author g13951te
+ */
+public class FirstHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+}
